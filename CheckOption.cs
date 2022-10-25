96085e067eb0f13e6cb0768d6bf66a6947a80c27
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-25 19:45:10 +0530

    added Player Position Check Program

---
+++ CheckOption.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class CheckOption
+    {
+        public static void Check()
+        {
+            int IfLadder = 1;
+            int IfSnake = 2;
+            int PlayerPosition = 0;
+            Random random = new Random();   
+            int dice = random.Next(1,7);
+            int option = random.Next(0,3);
+
+            Console.WriteLine("Dice Roll Number : " + dice);
+
+            if(option == IfLadder)
+            {
+                Console.WriteLine("Dice Number is " + dice);
+                Console.WriteLine("Player Position is Ladder");
+                PlayerPosition +=dice;
+            }
+            else if (option == IfSnake)
+            {
+                Console.WriteLine("Dice Number is " + dice);
+                Console.WriteLine("Player Position is Snake");
+                PlayerPosition -= dice;
+            }
+            else
+            {
+                Console.WriteLine("Player is No Play");
+            }
+        }
+    }
+}
