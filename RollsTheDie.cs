0638329eb1f78b942693530934bf15f131d749fb
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-25 19:41:55 +0530

    added Player rolling the dice Program

---
+++ RollsTheDie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class RollsTheDie
+    {
+        public static void Check()
+        {
+            Random random = new Random();
+            int die = random.Next(1,7);
+            Console.WriteLine("Rolling The Die : " + die);
+        }
+    }
+}
