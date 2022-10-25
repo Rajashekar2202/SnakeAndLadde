e73b5ae46bd3b9234b6d06244d6ebf92e23f6d76
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-25 19:53:59 +0530

    added Number of the times the dice played

--- ReportingDetails.cs
+++ ReportingDetails.cs
@@ -6,29 +6,29 @@ using System.Threading.Tasks;
 
 namespace SnakeAndLadder
 {
-    public class ExactWinningPosition
+    public class ReportingDetails
     {
         public static void Check()
         {
-            int StartPosition = 0;
+            
             int CurrentPosition = 0;
             int IfLadder = 0;
             int IfSanke = 0;
 
             Console.WriteLine("Dice Roll Number : ");
 
-            while(CurrentPosition < 100)
+            while (CurrentPosition < 100)
             {
                 Random random = new Random();
                 int dice = random.Next(1, 7);
-                int option = random.Next(0,3);
-                
-                if(option == IfLadder)
+                int option = random.Next(0, 3);
+
+                if (option == IfLadder)
                 {
                     Console.WriteLine("Player Position is Ladder");
                     CurrentPosition += dice;
                 }
-                else if(option == IfSanke)
+                else if (option == IfSanke)
                 {
                     Console.WriteLine("Player Postion is Snake");
                     CurrentPosition -= dice;
@@ -37,16 +37,11 @@ namespace SnakeAndLadder
                 {
                     Console.WriteLine("Player is No Play");
                 }
-                if(CurrentPosition > StartPosition)
-                {
-                    CurrentPosition -= StartPosition;
-                }
-                if(CurrentPosition > 100)
-                {
-                    CurrentPosition = 100;
-                }
+
+                Console.WriteLine("Player Final Position after Play: " + CurrentPosition);
+                Console.WriteLine("Number of Times The Die was Played to Win The Game: " );
+
             }
-            Console.WriteLine("Player Final Position after Play : " + CurrentPosition);
         }
     }
 }
